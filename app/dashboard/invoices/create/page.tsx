6372c464601ebc7247ceae0777b35ff6f6d14ff6
Alejandro <alejandroisa71@gmail.com>
2024-01-21 02:35:59 -0300

    Add funcionality post in Invoices Table

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,6 +1,23 @@
-const CreatePage = () => {
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page() {
+  const customers = await fetchCustomers();
+ 
   return (
-    <div>CreatePage</div>
-  )
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
 }
-export default CreatePage
