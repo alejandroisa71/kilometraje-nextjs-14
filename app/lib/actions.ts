b3750f0740c32806c0d741ef6cdac275af7f1059
Alejandro <alejandroisa71@gmail.com>
2024-01-21 12:42:25 -0300

    Add funcionality Updating and Deliting

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,7 +6,7 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-
+//TODO validate status
 const CreateInvoiceSchema = z.object({
     id: z.string(),
     customerId: z.string(),
@@ -41,3 +41,44 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices')
 
 }
+
+// Use Zod to update the expected types
+const UpdateInvoiceSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+
+const UpdateInvoice = UpdateInvoiceSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+//   console.log(amount, status);
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  }
