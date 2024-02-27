4b4c3521ff9589b29c70878c65eb516af322fa33
Tucuman <alejandoisa71@.gmail.com>
2024-02-27 20:04:16 -0300

    Continue configuration authentication

--- middleware..ts
+++ middleware..ts
@@ -2,27 +2,22 @@
 // import NextAuth from 'next-auth';
 // import { authConfig } from './auth.config';
 
-
 // export const { auth: middleware } = NextAuth(authConfig)
- 
+
 // export default NextAuth(authConfig).auth;
 
-
- 
 // export const config = {
 //   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 //   // matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 //   matcher: '/dashboard/:path*',
 // };
 
+import { auth } from './auth';
 
+export default auth((req) => {
+  console.log('ROUTE', req.nextUrl.pathname);
+});
 
-import {auth} from './auth'
-
-export default auth(req =>{
-  console.log("ROUTE", req.nextUrl.pathname)
-})
-
-export const config ={ 
-  matcher:["/auth/login", "/auth/register"]
-}
+export const config = {
+  matcher: ['/auth/login', '/auth/register'],
+};
